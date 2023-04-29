f07ea81d9da341d51226e8b16e097f25f0991696
kosmp <kosmin.2003@mail.ru>
2023-04-29 18:36:47 +0300

    added middleware that validates the token when the request is made

---
+++ server/src/middlewares/authMiddleware.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import {Request, Response, NextFunction} from 'express';
+const ApiError = require('../exceptions/api-error');
+const tokenService = require('../service/token-service');
+
+interface RequestWithUser extends Request {
+    user: any;  // any - temp type
+  }
+
+module.exports = function (req: RequestWithUser, res: Response, next: NextFunction) {
+    try {
+        const authorizationHeader = req.headers.authorization;
+        if (!authorizationHeader) {
+            return next(ApiError.UnauthorizedError());
+        }
+
+        const accessToken = authorizationHeader.split(' ')[1];
+        if (!accessToken) {
+            return next(ApiError.UnauthorizedError());
+        }
+
+        const userData = tokenService.validateAccessToken(accessToken);
+        if (!userData) {
+            return next(ApiError.UnauthorizedError());
+        }
+
+        req.user = userData;
+        next();
+    } catch (e) {
+        return next(ApiError.UnauthorizedError());
+    }
+};
